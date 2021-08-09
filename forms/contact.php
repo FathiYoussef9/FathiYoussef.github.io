c4195de2b29bde9370cf5ecee118b29ef37676dd
Fathi youssef <81202992+FathiYoussef9@users.noreply.github.com>
2021-08-09 16:02:27 +0100

    myresume

--- forms/contact.php
+++ forms/contact.php
@@ -7,6 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
+
   $receiving_email_address = 'fathiyoussef.dev@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
